dfa2b3fe7cae1f989e51def016c0fee3424d3ac5
souiai <merge.tool>
2022-07-16 00:39:58 +0200

    teste

--- models/rounds.py
+++ models/rounds.py
@@ -7,9 +7,7 @@ class Rounds:
 
     def sort_by_rating(self,list_players=[]):
         for i in range(len(list_players)):
-            j=i
-            while j>0 in range(len(list_players)):
-                list_players[i].player_ranking
+
 
     def first_rounds(self):
         pass
