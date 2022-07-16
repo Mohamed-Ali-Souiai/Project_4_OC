3c19857c12a14a954c2e37ecb43bfb835ab62e0e
souiai <merge.tool>
2022-07-16 08:53:55 +0200

    teste

--- models/rounds.py
+++ models/rounds.py
@@ -6,19 +6,20 @@ class Rounds:
     def __init__(self):
         pass
 
-    def sort_by_rating(self, list_players=[]):
+    def sort_by_rating(self, list_players):
         for i in range(len(list_players)):
             j = i
             while j > 0 and list_players[j-1].player_ranking > list_players[j].player_ranking:
-                list_players[j-1].player_ranking, list_players[j].player_ranking = list_players[j].player_ranking, list_players[j-1].player_ranking
+                list_players[j-1], list_players[j] = list_players[j], list_players[j-1]
                 j -= 1
         return list_players
 
-    def first_rounds(self, list_players=[], list_rounds=[]):
+    def first_rounds(self, list_players, list_rounds=None):
         for i in range(4):
-            list_rounds[i] = list_players[i],list_players[i+4]
+            list_rounds[i] = list_players[i], list_players[i+4]
+        return list_rounds
 
-    def second_rounds(self, list_players=[], list_rounds=[]):
+    def second_rounds(self, list_players, list_rounds=None):
         j = 0
         for i in range(2):
             list_rounds[i] = list_players[j], list_players[j + 1]
