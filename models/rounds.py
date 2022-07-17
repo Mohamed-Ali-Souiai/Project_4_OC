0c02d70f2dd7a1a22fec4d463063cb058af78ae2
souiai <merge.tool>
2022-07-18 00:30:38 +0200

    avancement sur la projet 4

--- models/rounds.py
+++ models/rounds.py
@@ -20,7 +20,9 @@ class Rounds:
         return list_rounds
 
     def next_rounds(self, list_players, list_rounds=None):
-        j = 0
-        for i in range(2):
-            list_rounds[i] = list_players[j], list_players[j + 1]
-            j += 2
+        index = 0
+        copy_list_players = list_players.copy()
+        while len(copy_list_players) > 0:
+            match = copy_list_players.pop(index), copy_list_players.pop(index)
+            list_rounds.append(match)
+        return list_rounds
