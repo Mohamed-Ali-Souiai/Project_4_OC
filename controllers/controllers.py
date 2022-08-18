1c027652343091ed6ce6946e8dfc62d440be6dc3
souiai <merge.tool>
2022-08-18 23:07:21 +0200

    executable application

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -284,7 +284,7 @@ class Controllers:
             elif menu == '4':  # "afficher les résultats"
                 self.view.show_results(self.tournament.results)
             elif menu == '5':  # "sauvegader les donnes du tournoi"
-                if self.tournament.rounds_number in [4, 3, 2, 1]:
+                if self.tournament.rounds_number in [3, 2, 1, 0]:
                     self.menu.data_logging(self.tournament, self.tournament.tournament_name)
             elif menu == '6':  # "Liste de tous les joueurs du tournoi "
                 self.menu.List_tournament_players(self.players)
